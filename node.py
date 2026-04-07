eed2ff73638e080eb9c432c7cb2892d9d65e98e8
genggreta2026 <genggreta1@gmail.com>
2026-04-07 12:20:43 +0800

    Add files via upload

---
+++ node.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+from blockchain import Blockchain
+
+app = Flask(__name__)
+blockchain = Blockchain()
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
